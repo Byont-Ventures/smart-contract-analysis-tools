526a3d892a72d8dd6bde999732cc55de176f0a3b
enzoevers <enzoeers@hotmail.com>
2022-11-07 07:08:24 +0000

    feat: WIP kevm in submodule

--- README.md
+++ README.md
@@ -18,6 +18,8 @@ $ source ~/.bashrc
 
 ## Usage
 
+### Security scans and report generation
+
 Copy [analysis-config-example.toml](./analysis-config-example.toml) and update as required.
 
 The configuration of for the report generation is done in a `.toml` file. This chosen over json mainly because both Foundry and Rust use toml files for their configuration as well. On top of that, toml supports comments while json doesn't.
@@ -27,3 +29,7 @@ $ yarn --cwd <path to this folder> run  \
     scan:generate-report                \
     <absolute path to config file>
 ```
+
+### KEVM
+
+While most tools don't require any user input other than some flags and the source files, kevm is different. For it to work you have to define for each function the expected state with optional pre- and postconditions. Additionally, sometimes you have to add custom rules for the analysis to work. Due to the manual changes required, the specifications for the contracts should be created outside of the submodule (**or docker image if we want to use that instead**). The path to these specifications should be configured under the `[kevm]` section at `kevm_spec_rel_path` in the `analysis.toml` file.
--- kevm/run-VeriToken-spec.sh
+++ kevm/run-VeriToken-spec.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 projectRoot=$1
-contractName=$2
+pathToSecurityScansFromRoot=$2
+pathToSourceFileFromRoot=$3
+contractName=$4
 
-echo ""
-echo "================================================================="
-echo "Setup the local environment"
-echo "================================================================="
-echo ""
-
-cd ${projectRoot}
-projectRoot=${PWD}
-
-cd ${projectRoot}/smart-contracts
+if [ -z "${contractName}" ]
+then
 
 # If you get an error saying: ERROR: [Errno 2] No such file or directory: 'install'
 # Do the following: (https://stackoverflow.com/questions/46013544/yarn-install-command-error-no-such-file-or-directory-install)
@@ -23,58 +17,57 @@ cd ${projectRoot}/smart-contracts
 #    apt update
 #    apt install yarn -y
 
-yarn install
-git submodule update --init --recursive -- lib/forge-std 
-cd ${projectRoot}
+mkdir -p $(dirname "$0")/results/${contractName}
+outputFile=$(dirname "$0")/results/${contractName}/${contractName}-kevm.result
 
-echo ""
-echo "================================================================="
-echo "Flatten the contract to be verified"
-echo "================================================================="
-echo ""
+echo ""                                                                     | tee ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo "Flatten the contract to be verified"                                  | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo ""                                                                     | tee -a ${outputFile}
 
 docker run --rm -v ${projectRoot}:/prj ghcr.io/foundry-rs/foundry:latest "  \
-    cd /prj/smart-contracts                                                 \
-    && forge flatten src/${contractName}.sol                                \
-    --output ../flattened/${contractName}-flat.sol"
+    cd /prj/                                                                \
+    && forge flatten ${pathToSourceFileFromRoot}/${contractName}.sol        \
+    --output ${pathToSecurityScansFromRoot}/flattened/${contractName}-flat.sol" 2>&1 | tee -a ${outputFile}
 
-echo ""
-echo "================================================================="
-echo "Generate helper modules for kevm to make writing claims easier"
-echo "================================================================="
-echo ""
+echo ""                                                                     | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo "Generate helper modules for kevm to make writing claims easier"       | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo ""                                                                     | tee -a ${outputFile}
 
-docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/kevm:latest bash -c "                 \
-    mkdir -p /prj/kevm/generated                                                                    \
-    && kevm solc-to-k /prj/flattened/${contractName}-flat.sol ${contractName}                       \
-    --pyk --verbose --profile --verbose --definition root/evm-semantics/.build/usr/lib/kevm/haskell \
-    --main-module ${contractName}-VERIFICATION                                                      \
-    > /prj/kevm/generated/${contractName}-bin-runtime.k"
+docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/kevm:latest bash -c "                             \
+    mkdir -p /prj/${pathToSecurityScansFromRoot}/kevm/generated                                                 \
+    && kevm solc-to-k /prj/${pathToSecurityScansFromRoot}/flattened/${contractName}-flat.sol ${contractName}    \
+    --pyk --verbose --profile --verbose --definition root/evm-semantics/.build/usr/lib/kevm/haskell             \
+    --main-module ${contractName}-VERIFICATION                                                                  \
+    > /prj/${pathToSecurityScansFromRoot}/kevm/generated/${contractName}-bin-runtime.k" 2>&1 | tee -a ${outputFile}
 
-echo ""
-echo "================================================================="
-echo "Generate the required files for verification"
-echo "================================================================="
-echo ""
+echo ""                                                                     | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo "Generate the required files for verification"                         | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo ""                                                                     | tee -a ${outputFile}
 
 # Whenever you change the specifications, run this command again.
-docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/kevm:latest bash -c "         \
-    kevm kompile --backend haskell /prj/kevm/${contractName}-spec.md                        \
-        --definition /prj/kevm/generated/${contractName}-spec/haskell                       \
-        --main-module VERIFICATION                                                          \
-        --syntax-module VERIFICATION                                                        \
-        --concrete-rules-file /root/evm-semantics/tests/specs/examples/concrete-rules.txt   \
-        -I root/evm-semantics/.build/usr/lib/kevm/include/kframework                        \
-        -I root/evm-semantics/.build/usr/lib/kevm/blockchain-k-plugin/include/kframework    \
-        --verbose"
+docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/kevm:latest bash -c "                     \
+    kevm kompile --backend haskell /prj/${pathToSecurityScansFromRoot}/kevm/${contractName}-spec.md     \
+        --definition /prj/${pathToSecurityScansFromRoot}/kevm/generated/${contractName}-spec/haskell    \
+        --main-module VERIFICATION                                                                      \
+        --syntax-module VERIFICATION                                                                    \
+        --concrete-rules-file /root/evm-semantics/tests/specs/examples/concrete-rules.txt               \
+        -I root/evm-semantics/.build/usr/lib/kevm/include/kframework                                    \
+        -I root/evm-semantics/.build/usr/lib/kevm/blockchain-k-plugin/include/kframework                \
+        --verbose" 2>&1 | tee -a ${outputFile}
 
-echo ""
-echo "================================================================="
-echo "Verify the the Solidity contract"
-echo "================================================================="
-echo ""
+echo ""                                                                     | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo "Verify the the Solidity contract"                                     | tee -a ${outputFile}
+echo "================================================================="    | tee -a ${outputFile}
+echo ""                                                                     | tee -a ${outputFile}
 
-docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/kevm:latest bash -c " \
-    kevm prove --backend haskell /prj/kevm/${contractName}-spec.md                  \
-        --definition /prj/kevm/generated/${contractName}-spec/haskell               \
-        --verbose"
+docker run --rm -v ${projectRoot}:/prj ghcr.io/byont-ventures/kevm:latest bash -c "                     \
+    kevm prove --backend haskell /prj/${pathToSecurityScansFromRoot}/kevm/${contractName}-spec.md       \
+        --definition /prj/${pathToSecurityScansFromRoot}/kevm/generated/${contractName}-spec/haskell    \
+        --verbose" 2>&1 | tee -a ${outputFile}
